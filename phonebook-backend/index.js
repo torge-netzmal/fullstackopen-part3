cf875ec0866584afaa5225ea3f234be09b1fb2cb
Torge Schöwing <ts@netzmal.de>
2026-01-25 22:22:48 +0100

    Ex 3.7, Ex.3.8

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,7 +1,24 @@
 const express = require('express')
+const morgan = require('morgan')
+
+
 const app = express()
 
 app.use(express.json())
+
+app.use(morgan(function (tokens, req, res) {
+    const log = [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms'
+    ];
+    if (req.method === 'POST') {
+        log.push(JSON.stringify(req.body));
+    }
+    return log.join(' ')
+}))
 
 let persons = [
     {
@@ -100,6 +117,12 @@ app.post('/api/persons', (request, response) => {
 
     response.json(person)
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({error: 'unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
