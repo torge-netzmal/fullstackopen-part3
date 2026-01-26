af7f101fe9488eb9ff87a75b94c3bfc8f7cd43f8
Torge Schöwing <ts@netzmal.de>
2026-01-26 18:52:03 +0100

    3b

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 
 let notes = [
@@ -73,7 +75,7 @@ app.post('/api/notes', (request, response) => {
     response.json(note)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
